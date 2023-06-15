7030b30007ebd9e9044ed05a395e8164880713fc
Qui Nguyen <quinguyen.dev@gmail.com>
2023-06-15 14:27:42 -0500

    [✅] REVOLVING TORUS BABYY.

--- src/pages/RevolvingAsset/RevolvingAsset.tsx
+++ src/pages/RevolvingAsset/RevolvingAsset.tsx
@@ -1,3 +1,37 @@
+import { useRef } from "react";
+
+/* https://docs.pmnd.rs/react-three-fiber/tutorials/basic-animations */
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Mesh } from "three";
+
+function RotatingTorusWireframe() {
+    const mesh = useRef<Mesh>(null!);
+
+    useThree(({ camera }) => {
+        camera.position.z = 40;
+    });
+
+    useFrame(({ clock }) => {
+        mesh.current.rotation.x = clock.getElapsedTime() * 0.5;
+        mesh.current.rotation.y = clock.getElapsedTime() * 0.1;
+    });
+
+    return (
+        <mesh ref={mesh}>
+            <torusGeometry args={[10, 4, 16, 50]} />
+            <meshPhysicalMaterial wireframe={true} />
+        </mesh>
+    );
+}
+
 export default function RevolvingAsset() {
-    return <div>RevolvingAsset</div>;
+    return (
+        <div className="ra-container">
+            <Canvas>
+                <RotatingTorusWireframe />
+                <ambientLight />
+                <directionalLight color="white" position={[0, 100, 20]} />
+            </Canvas>
+        </div>
+    );
 }
