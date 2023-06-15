db4017bd4f283064ce25ebe22e106660c87c89db
Qui Nguyen <quinguyen.dev@gmail.com>
2023-06-14 19:41:03 -0500

    [🛠] Fixed up the routing to use objects and added splash route for a 404.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import React, { Profiler, ProfilerOnRenderCallback } from "react";
-import {
-    Route,
-    Routes,
-    RouterProvider,
-    createBrowserRouter,
-    createRoutesFromElements,
-} from "react-router-dom";
-import { Home } from "src/pages";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home, NoPageFound } from "src/pages";
 
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Home />}>
-            <Route path="*" element={<Home />} />
-        </Route>
-    )
-);
+const router = createBrowserRouter([
+    { index: true, Component: Home },
+    { path: "*", Component: NoPageFound },
+]);
 
 export default function App() {
     return (
-        // <Profiler id="app" onRender={callback}>
         <main>
             <RouterProvider router={router} />
         </main>
-        // </Profiler>
     );
 }
+
+// TODO check if I semantically need main here ?
