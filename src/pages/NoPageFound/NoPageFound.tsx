0c4f8990a8187f02a394018e9c767a9127493e97
Qui Nguyen <quinguyen.dev@gmail.com>
2023-06-14 20:26:13 -0500

    [🚧] Added a basic 404 page and made a quick style for buttons.

--- src/pages/NoPageFound/NoPageFound.tsx
+++ src/pages/NoPageFound/NoPageFound.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function NoPageFound() {
-    return <div>NoPageFound</div>;
+    return (
+        <div className="no-page-container">
+            <h1>woah... what do you think ur doing ? 🤔</h1>
+            <Link to="/" className="button">
+                return home
+            </Link>
+        </div>
+    );
 }
