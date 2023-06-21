3773ab747aca1dc42f55344432384e7da34415d6
Qui Nguyen <quinguyen.dev@gmail.com>
2023-06-21 13:50:40 -0500

    [🆕] Creating .tsx page for HtmlRender

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,6 +15,9 @@ export default function Home() {
                     <li>
                         <Link to="/revolving-asset">revolving asset</Link>
                     </li>
+                    <li>
+                        <Link to="/html-render">html on 3d component</Link>
+                    </li>
                 </ul>
             </nav>
         </>
---
+++ src/pages/HtmlRender/HtmlRender.tsx
@@ -0,0 +1,9 @@
+import { Canvas } from "@react-three/fiber";
+
+export default function HtmlRender() {
+    return (
+        <Canvas>
+            <></>
+        </Canvas>
+    );
+}
