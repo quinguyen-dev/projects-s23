635efa58113f8fec3feaf344ab92de36e231a24f
Qui Nguyen <quinguyen.dev@gmail.com>
2023-06-26 12:09:01 -0500

    [🆕] Added .eslintrc.json and fixed up some files that were conflicting with linter. Added prettier extension for lint.

--- src/utils/tests/three.utils.ts
+++ src/utils/tests/three.utils.ts
@@ -1,12 +1,15 @@
-import {
+import type {
     Renderer,
     TreeNode,
 } from "@react-three/test-renderer/dist/declarations/src/types";
 
 /* TODO:  rework this function */
-export function findComponentMesh(renderer: Renderer, type: string) {
+export function findComponentMesh(
+    renderer: Renderer,
+    type: string
+): TreeNode | undefined {
     const tree = renderer.toTree();
+    const mesh = tree?.find((child: TreeNode) => child.type === "mesh");
 
-    let mesh = tree?.find((child: TreeNode) => child.type == "mesh");
     return mesh?.children.find((mesh) => mesh.type === type);
 }
